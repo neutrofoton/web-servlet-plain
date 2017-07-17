4d892c4ab8088bd6a445aec186c4aefb5a7628f0
neutro <neutrofoton@gmail.com>
2017-07-17 17:43:03 +0700

    Migrate content to basic-spring-boot repository

--- spring-boot-journal/src/main/java/com/neutrofoton/spring/repository/JournalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.neutrofoton.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.neutrofoton.spring.domain.Journal;
-
-public interface JournalRepository extends JpaRepository<Journal,Long>{
-
-}
